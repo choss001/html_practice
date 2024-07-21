e314c199a62758a252472229094315468543e39c
chloe <thisis_chloe@naver.com>
2024-07-22 01:27:55 +0900

    css scroll practice

--- css_practice/test.js
+++ css_practice/test.js
@@ -1,6 +1,15 @@
+var scrollinterval
+var scrollspeed = 1;
+
 function scrollmenow(direction){
+    document.getElementById("top-nav-bar").scrollLeft += 50
     scrollinterval = window.setInterval(function() {
-        console.log("test");
+    document.getElementById("top-nav-bar").scrollLeft += 10
+    console.log("test scroll left = "+document.getElementById("top-nav-bar").scrollLeft);
+        scrollspeed = scrollspeed * 1.1;
+/*        if (scrollspeed > 10) {scro}
+        document.getElementById("scroll_left_btn").scrollLeft
+        console.log("test");*/
 
     },10);
     console.log(direction)
@@ -10,4 +19,8 @@ function test(){
     test = window.setInterval(function() {
         console.log("tset!!!")
     }, 10)
+}
+function stopscrollmenow(){
+    scrollspeed = 1;
+    window.clearInterval(scrollinterval);
 }
