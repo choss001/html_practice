1a5989eda937804402e0c47398df7341d7ee30a4
choss001 <choss01@naver.com>
2024-07-22 14:44:46 +0900

    css practice expectially part of scroll

--- css_practice/test.js
+++ css_practice/test.js
@@ -2,17 +2,18 @@ var scrollinterval
 var scrollspeed = 1;
 
 function scrollmenow(direction){
-    document.getElementById("top-nav-bar").scrollLeft += 50
     scrollinterval = window.setInterval(function() {
-    document.getElementById("top-nav-bar").scrollLeft += 10
-    console.log("test scroll left = "+document.getElementById("top-nav-bar").scrollLeft);
         scrollspeed = scrollspeed * 1.1;
-/*        if (scrollspeed > 10) {scro}
-        document.getElementById("scroll_left_btn").scrollLeft
-        console.log("test");*/
+        if (scrollspeed > 15) {
+            scrollspeed = 15;
+        }
+        if (direction == 1){
+            document.getElementById("top-nav-bar").scrollLeft += scrollspeed;
+        }else{
+            document.getElementById("top-nav-bar").scrollLeft -= scrollspeed;
+        }
 
     },10);
-    console.log(direction)
 }
 
 function test(){
