37bee592700f6658e04b858d7ffcc84d8a597d67
choss001 <choss01@naver.com>
2024-07-24 14:16:26 +0900

    typescript practice

--- css_practice/test.js
+++ css_practice/test.js
@@ -12,16 +12,42 @@ function scrollmenow(direction){
         }else{
             document.getElementById("top-nav-bar").scrollLeft -= scrollspeed;
         }
-
+        scrollbtn_visible();
     },10);
+
 }
 
 function test(){
     test = window.setInterval(function() {
-        console.log("tset!!!")
     }, 10)
 }
 function stopscrollmenow(){
     scrollspeed = 1;
     window.clearInterval(scrollinterval);
+}
+
+
+var p_s_l = 0;
+var x_s_l = 0;
+function scrollbtn_visible() {
+  let c_s_l = document.getElementById("top-nav-bar").scrollLeft;
+  if (c_s_l < 1) {
+    document.getElementById("btn_container_subtopnav_left").style.display = "none";    
+  } else {
+    document.getElementById("btn_container_subtopnav_left").style.display = "block";    
+  }
+  console.log("c_s_l = " + c_s_l)
+  console.log("p_s_l = " + p_s_l)
+  console.log("x_s_l = " + x_s_l)
+  if (c_s_l > 1 && c_s_l == p_s_l) {
+    x_s_l++;
+  } else {
+    x_s_l = 0;  
+  }
+  if (x_s_l > 3){
+    document.getElementById("btn_container_subtopnav_right").style.display = "none";    
+  } else {
+    document.getElementById("btn_container_subtopnav_right").style.display = "block";  
+  }
+  p_s_l = document.getElementById("top-nav-bar").scrollLeft;
 }
